f7662c51ce34a48446b2d7bbf181bbba50a9a128
Shashank Tiwari <106220488+shashankktiwariii@users.noreply.github.com>
2022-10-19 21:55:14 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -21,7 +21,7 @@ $disqus = "https://random-co6vnxqche.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://api.anikatsu.ga"; // Github - https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://266f7b13-4867-4ef5-bd86-b25dc799681b.id.repl.co"; // https://replit.com/@shashankktiwari/anikatsu
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
